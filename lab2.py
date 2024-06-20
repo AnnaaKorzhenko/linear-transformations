fdebda157fe175539eb5ff338c4418484771d51a
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-06-20 17:57:14 +0300

    pca completed!!

--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.image import imread
+import matplotlib.pyplot as plt
+from skimage import io
 
 
 # func for finding the eigenvectors and eigenvalues 
@@ -23,9 +24,69 @@ def find_eigen(matrix):
     return eigenvalues, eigenvectors
 
 # original image
-image_in_color = imread("myimage.jpg")
+image_color = imread("myimage.jpg")
 
-print(f"Висота, ширина, канали): {image_in_color.shape}")
-plt.imshow(image_in_color)
+print(f"Висота, ширина, канали): {image_color.shape}")
+plt.imshow(image_color)
 plt.axis('off')
+plt.show()
+
+# making it bw
+image_sum = image_color.sum(axis=2)
+image_bw = image_sum / image_color.shape[2]
+bw_height, bw_width = image_bw.shape
+print(f"Розміри чорно-білого зображення: {bw_height}, {bw_width}")
+print(f"Кількість каналів кольорів після перетворення в чорно-біле:", 1)
+
+plt.imshow(image_bw, cmap='gray')
+plt.axis('off')
+plt.show()
+
+# centering
+image_bw_centered = image_bw - np.mean(image_bw)
+
+# covariance matrix
+cov_matrix = np.cov(image_bw_centered, rowvar=False)
+
+# eigen
+eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+# sort eigen
+sorted_index = np.argsort(eigenvalues)[::-1]
+sorted_eigenvalues = eigenvalues[sorted_index]
+sorted_eigenvectors = eigenvectors[:, sorted_index]
+
+# finding number of components for 95% variance
+cumulative_variance = np.cumsum(sorted_eigenvalues) / np.sum(sorted_eigenvalues)
+num_components_95 = np.argmax(cumulative_variance >= 0.95)
+print(f"Number of components to cover 95% variance: {num_components_95}")
+
+plt.plot(cumulative_variance, marker='o')
+plt.axhline(y=0.95, color='r', linestyle='--')
+plt.axvline(x=num_components_95, color='r', linestyle='--')
+plt.title('Cumulative Variance Explained by PCA Components')
+plt.xlabel('Number of Components')
+plt.ylabel('Cumulative Variance')
+plt.show()
+
+# екфтащкьштп вфеф
+pca_components = sorted_eigenvectors[:, :num_components_95]
+image_bw_pca = np.dot(image_bw_centered, pca_components)
+
+# reconstruction
+image_bw_reconstructed_flat = np.dot(image_bw_pca, pca_components.T)
+image_bw_reconstructed = image_bw_reconstructed_flat.reshape(image_bw.shape)
+
+# showing diff components
+components_list = [10, 20, num_components_95, 50, 100, 500]
+
+for i, num_components in enumerate(components_list, 1):
+    pca_components = sorted_eigenvectors[:, :num_components]
+    image_bw_pca = np.dot(image_bw_centered, pca_components)
+    image_bw_reconstructed_flat = np.dot(image_bw_pca, pca_components.T)
+    image_bw_reconstructed = image_bw_reconstructed_flat.reshape(image_bw.shape)
+
+    plt.subplot(2, 3, i)
+    plt.imshow(image_bw_reconstructed, cmap='gray')
+    plt.title(f'{num_components} Components')
+    plt.axis('off')
 plt.show()
